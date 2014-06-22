84698b1b9df921042bee43aee91322c7554eda4f
Matthew Creager <mattcreager@goinstant.com>
2014-06-22 15:08:28 -0300

    Swapping in symlinks

--- level-1/client/common/goinstant-config.js
+++ level-1/client/common/goinstant-config.js
@@ -1,16 +1,1 @@
-/* global module */
-
-'use strict';
-
-var CONNECT_URL = 'https://goinstant.net/mattcreager/soapbox-dev';
-
-
-module.exports = function($goConnectionProvider) {
-  var origin = window.location.origin;
-  var path = window.location.pathname;
-  var returnTo = origin + path;
-
-  $goConnectionProvider.$set(CONNECT_URL);
-  $goConnectionProvider.$loginUrl(['Twitter']);
-  $goConnectionProvider.$logoutUrl(returnTo);
-};
+../../../final/client/common/goinstant-config.js
